a4e746f1766b5e57d65e11f09b5b9048a47c13e7
kafribung <60900117001@uin-alauddin.ac.id>
2021-01-04 10:59:22 +0800

    fix clear bug script dash

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,5 +1,6 @@
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js "></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js "></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js "></script>
 <script src="{{ asset('dash/assets/js/main.js') }} "></script>
