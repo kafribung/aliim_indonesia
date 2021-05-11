35d1d9dc45d5883819eab71b508b572271a05cd5
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-11 15:08:46 +0800

    risize image

--- config/app.php
+++ config/app.php
@@ -162,8 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         // Image intervention
-        Intervention\Image\ImageServiceProvider::class,
-
+        // Intervention\Image\ImageServiceProvider::class,
 
         // Captcha
 
@@ -231,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        // 'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
