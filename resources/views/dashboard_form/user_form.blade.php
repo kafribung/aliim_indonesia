dfc974ee11e6fa428446a56f5ee762acf5fc1f67
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-12 16:13:27 +0800

    clear bugs

--- resources/views/dashboard_form/user_form.blade.php
+++ resources/views/dashboard_form/user_form.blade.php
@@ -3,10 +3,7 @@
     <label for="img" class="control-label mb-1">Foto</label>
     <img src="{{ url($user->takeImg) }}" alt="Gambar {{$user->name}}"
         title="Gambar {{$user->name}}" width="100" height="100">
-    <input id="img" name="img" type="file"
-        class="form-control @error('img') is-invalid @enderror" autofocus required
-        autocomplete="off"">
-
+    <input id="img" name="img" type="file" class="form-control @error('img') is-invalid @enderror" autofocus required autocomplete="off"">
     @error('img')
         <p class=" alert alert-danger">{{$message}}</p>
     @enderror
@@ -14,10 +11,7 @@
 
 <div class="form-group">
     <label for="name" class="control-label mb-1">nama</label>
-    <input id="name" name="name" type="text"
-        class="form-control @error('name') is-invalid @enderror" required
-        autocomplete="off" value="{{old('name') ?? $user->name}}">
-
+    <input id="name" name="name" type="text" class="form-control @error('name') is-invalid @enderror" required autocomplete="off" value="{{old('name') ?? $user->name}}">
     @error('name')
     <p class="alert alert-danger">{{$message}}</p>
     @enderror
@@ -25,10 +19,7 @@
 
 <div class="form-group">
     <label for="email" class="control-label mb-1">email</label>
-    <input id="email" name="email" email="email"
-        class="form-control @error('email') is-invalid @enderror" autocomplete="off"
-        required value="{{old('email') ?? $user->email}}">
-
+    <input id="email" name="email" email="email" class="form-control @error('email') is-invalid @enderror" autocomplete="off" required value="{{old('email') ?? $user->email}}">
     @if ($errors->has('email'))
     <p class="alert alert-danger">{{$errors->first('email')}}</p>
     @endif
@@ -36,11 +27,7 @@
 
 <div class="form-group">
     <label for="date_birth" class="control-label mb-1">Tgl Lahir</label>
-    <input id="date_birth" name="date_birth" type="date"
-        class="form-control @error('date_birth') is-invalid @enderror" required
-        autocomplete="off"
-        value="{{old('date_birth') ?? $user->date_birth}}">
-
+    <input id="date_birth" name="date_birth" type="date" class="form-control @error('date_birth') is-invalid @enderror" required autocomplete="off" value="{{old('date_birth') ?? $user->date_birth}}">
     @error('date_birth')
     <p class="alert alert-danger">{{$message}}</p>
     @enderror
