3dbfbbd0c2b2a1618d812d68359f81bd76085410
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-13 23:20:39 +0800

    ustad clean bug

--- resources/views/dashboard/ustad.blade.php
+++ resources/views/dashboard/ustad.blade.php
@@ -1,25 +1,21 @@
 @extends('layouts.master')
 @section('title', 'Ustad | Aliim Indonesia')
 @section('content')
-
 @include('dashboard_form.cari_form', ['data' => request()->path()])
 <!-- Content -->
 <div class="content">
     <!-- Animated -->
     <div class="animated fadeIn">
-
         @if (session('msg'))
         <p class="alert alert-info">{{session('msg')}}</p>
         @endif
-
         <div class="row">
             <div class="col-lg-12">
                 <div class="card-body ">
                     <div class="card">
                         <div class="card-header">
                             <h4 class="box-title text-center">Ustad Aliim</h4>
-                            <a href="/ustad/create" class="btn btn-primary btn-sm float-right"><i
-                                    class="fa fa-plus"></i></a>
+                            <a href="/ustad/create" class="btn btn-primary btn-sm float-right"><i class="fa fa-plus"></i></a>
                         </div>
                         <div class="table-stats">
                             <table class="table table-hover">
@@ -45,10 +41,7 @@
                                     <tr>
                                         <td>{{$angkaAwal++}}</td>
                                         <td class="avatar">
-                                            <div class="round-img">
-                                                <a href="#"><img class="rounded-circle" src="{{url($ustad->takeImg)}}"
-                                                        alt="Foto {{$ustad->name}}" title="Foto {{$ustad->name}}"></a>
-                                            </div>
+                                            <img class="lazy img-thumbnail" src="{{url($ustad->takeImg)}}" alt="Foto {{$ustad->name}}" title="Foto {{$ustad->name}}">
                                         </td>
                                         <td>{{$ustad->name}}</td>
                                         <td>{{$ustad->email}}</td>
@@ -59,19 +52,11 @@
                                         <td>{{($ustad->role == 0) ? 'User' : (($ustad->role == 1) ? 'Admin' : 'Ustad')}}
                                         </td>
                                         <td>
-                                            <a href="/ustad/{{$ustad->email}}/edit"
-                                                class="btn btn-warning btn-sm {{$ustad->status != 1 ? 'disabled' : ''}}"><i
-                                                    class="fa fa-edit"></i></a>
-
+                                            <a href="/ustad/{{$ustad->email}}/edit" class="btn btn-warning btn-sm {{$ustad->status != 1 ? 'disabled' : ''}}"><i class="fa fa-edit"></i></a>
                                             <form action="/ustad/{{$ustad->email}}" method="POST" class="d-inline-flex">
                                                 @csrf
                                                 @method('DELETE')
-
-                                                <button type="submit"
-                                                    onclick="return confirm('Hapus Data Ustad {{$ustad->name}}?')"
-                                                    class="btn btn-danger btn-sm"
-                                                    {{$ustad->status != 1 ? 'disabled' : ''}}><i
-                                                        class="fa fa-trash"></i></button>
+                                                <button type="submit" onclick="return confirm('Hapus Data Ustad {{$ustad->name}}?')" class="btn btn-danger btn-sm" {{$ustad->status != 1 ? 'disabled' : ''}}><i class="fa fa-trash"></i></button>
                                             </form>
                                         </td>
                                     </tr>
@@ -88,11 +73,9 @@
         <div class="row float-right">
             {{ $ustads->links('pagination::simple-bootstrap-4') }}
         </div>
-
         <!-- /#add-category -->
     </div>
     <!-- .animated -->
 </div>
 <!-- /.content -->
-
 @endsection
