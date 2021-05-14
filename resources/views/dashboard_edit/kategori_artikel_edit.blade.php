f863ac4226dc29555854a4ae48749ec99f71ea3c
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-14 14:41:32 +0800

    clear line

--- resources/views/dashboard_edit/kategori_artikel_edit.blade.php
+++ resources/views/dashboard_edit/kategori_artikel_edit.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.master')
 @section('title', 'Edit  Kategori Artikel | Aliim Indonesia')
 @section('content')
-
 <!-- Content -->
 <div class="content">
     <!-- Animated -->
     <div class="animated fadeIn">
-
         @if (session('msg'))
             <p class="alert alert-info">{{session('msg')}}</p>
         @endif
-
         <div class="row">
             <div class="col-lg-12">
                 <div class="card-body ">
@@ -22,19 +19,17 @@
                             <form action="/kategori-artikel/{{$kategori->id}}" method="POST">
                                 @csrf
                                 @method('PUT')
-                               
+                                @include('dashboard_form.kategori_artikel_form', ['kategori' => $kategori,  'create' => false])
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
         <!-- /#add-category -->
     </div>
     <!-- .animated -->
 </div>
 <!-- /.content -->
-    
 @endsection
 
