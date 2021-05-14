4f6cd76b75ca666f52c16c3b806b4d4bfecd83c0
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-14 20:31:09 +0800

    show name auth

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -16,24 +16,18 @@
                     <p>Hi, {{ Auth::user()->name }}</p>
                 </div>
             </div>
-
             <div class="user-area dropdown float-right">
                 <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="false">
                     <img class="user-avatar rounded-circle" src="{{ url(Auth::user()->takeImg) }}" alt="User Avatar">
                 </a>
-
                 <div class="user-menu dropdown-menu">
-                    <a class="nav-link" href=""
-                        onclick="event.preventDefault(); document.getElementById('logout').submit();"><i
-                            class="fa fa-power -off"></i>Logout</a>
-
+                    <a class="nav-link" href="" onclick="event.preventDefault(); document.getElementById('logout').submit();"><i class="fa fa-power -off"></i>Logout</a>
                     <form id="logout" action="{{ route('logout') }}" method="POST" style="d-none">
                         @csrf
                     </form>
                 </div>
             </div>
-
         </div>
     </div>
 </header>
