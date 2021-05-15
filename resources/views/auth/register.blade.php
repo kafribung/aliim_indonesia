18808b65fa3ac317132a0785e758bdb13a7ae425
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-15 13:27:12 +0800

    clear bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,21 +17,18 @@
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-				
 				<div class="wrap-input100 validate-input m-b-16">
 					<input class="input100" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
 					@error('email')
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-
 				<div class="wrap-input100 validate-input m-b-16">
 					<input class="input100" type="text" name="date_birth" placeholder="Tanggal Lahir" onfocus="(this.type='date')" value="{{ old('date_birth') }}" required >
 					@error('date_birth')
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-
 				<div class="wrap-input100 validate-input m-b-16">
 					<select name="gender" id="gender" class="form-control input100" style="padding: 5px" required>
 						<option value="Pria">Pria</option>
@@ -41,25 +38,21 @@
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-
 				<div id="app">
 					<district-component/>
 					@error('district')
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-				
 				<div class="wrap-input100 validate-input m-b-16">
 					<input class="input100" type="password" name="password" placeholder="Password"  required autocomplete="new-password">
 					@error('password')
 						<p class="alert alert-danger" style="color:#d9534f">{{ $message }}</p>
 					@enderror
 				</div>
-				
 				<div class="wrap-input100 validate-input m-b-16">
 					<input class="input100" type="password"  name="password_confirmation"  placeholder="Confirmation" required autocomplete="new-password">
 				</div>
-				
 				<div class="container-login100-form-btn">
 					<button type="submit" class="login100-form-btn">Daftar</button>
 				</div>
@@ -67,7 +60,6 @@
 					<span class="txt1 p-b-9">Sudah Punya Akun ?</span>
 					<a href="/login" class="txt3">Login Disini</a>
 				</div>
-
 				<div class=" m-b-16" data-validate = "Please enter captcha">
 					{!! RecaptchaV3::initJs() !!}
 					{!! RecaptchaV3::field('register') !!}
