a1f828b934160734b21599c530cce7e7bd88accc
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-15 08:14:01 +0800

    clear bug

--- resources/views/dashboard/hadist.blade.php
+++ resources/views/dashboard/hadist.blade.php
@@ -25,17 +25,17 @@
                                     @foreach ($hadists as $hadist)
                                         <tr>
                                             <th>No</th>
-                                            <td>{{$angkaAwal++}}</td>
+                                            <td>{{ $angkaAwal }}</td>
                                         </tr>
 
                                         <tr>
-                                            <th>Judul</th>
-                                            <td>{{$hadist->title}}</td>
+                                            <th>Tentang</th>
+                                            <td>{{ $hadist->title }}</td>
                                         </tr>
 
                                         <tr>
-                                            <th>Sanad</th>
-                                            <td>{{$hadist->info}}</td>
+                                            <th>Periwayat</th>
+                                            <td>{{ $hadist->info }}</td>
                                         </tr>
 
                                         <tr>
@@ -46,11 +46,11 @@
                                         <tr>
                                             <th>Action</th>
                                             <td>
-                                                <a href="/hadist/{{$hadist->id}}/edit" class="btn btn-warning btn-sm "><i class="fa fa-edit"></i></a>
-                                                <form action="/hadist/{{$hadist->id}}" method="POST" class="d-inline-flex">
+                                                <a href="/hadist/{{ $hadist->id }}/edit" class="btn btn-warning btn-sm "><i class="fa fa-edit"></i></a>
+                                                <form action="/hadist/{{ $hadist->id }}" method="POST" class="d-inline-flex">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" onclick="return confirm('Hapus Data hadist {{$hadist->title}}?')" class="btn btn-danger btn-sm" ><i class="fa fa-trash"></i></button>
+                                                    <button type="submit" onclick="return confirm('Hapus Data hadist {{ $hadist->title }}?')" class="btn btn-danger btn-sm" ><i class="fa fa-trash"></i></button>
                                                 </form>
                                             </td>
                                     </tr>
