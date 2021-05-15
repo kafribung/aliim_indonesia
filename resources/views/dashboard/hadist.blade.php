a92c8520cf4c5fad52a20c27c60cb8e73bc3cfc4
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-15 08:11:22 +0800

    clear line

--- resources/views/dashboard/hadist.blade.php
+++ resources/views/dashboard/hadist.blade.php
@@ -1,16 +1,13 @@
 @extends('layouts.master')
 @section('title', 'Hadist Harian | Aliim Indonesia')
 @section('content')
-
 <!-- Content -->
 <div class="content">
     <!-- Animated -->
     <div class="animated fadeIn">
-
         @if (session('msg'))
             <p class="alert alert-info">{{session('msg')}}</p>
         @endif
-
         <div class="row">
             <div class="col-lg-12">
                 <div class="card-body ">
@@ -67,11 +64,9 @@
                 </div>
             </div>
         </div>
-
         <!-- /#add-category -->
     </div>
     <!-- .animated -->
 </div>
 <!-- /.content -->
-    
 @endsection
--- resources/views/dashboard_form/hadist_form.blade.php
+++ resources/views/dashboard_form/hadist_form.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label for="title" class="control-label mb-1">Judul</label>
+    <label for="title" class="control-label mb-1">Tentang</label>
     <input id="title" name="title" type="text" class="form-control @error('title') is-invalid @enderror" autofocus  required autocomplete="off" value="{{old('title') ?? $hadist->title}}">
 
     @error('title')
@@ -8,7 +8,7 @@
 </div>
 
 <div class="form-group">
-    <label for="info" class="control-label mb-1">Sanad</label>
+    <label for="info" class="control-label mb-1">Periwayat</label>
     <input id="info" name="info" type="text" class="form-control @error('info') is-invalid @enderror"  required autocomplete="off" value="{{old('info') ?? $hadist->info}}">
 
     @error('info')
@@ -17,7 +17,7 @@
 </div>
 
 <div class="form-group">
-    <label for="description" class="control-label mb-1">Deskripsi</label>
+    <label for="description" class="control-label mb-1">Hadist</label>
     <textarea id="description" name="description" class="form-control ckeditor @error('description') is-invalid @enderror"  >{{old('description') ?? $hadist->description}}</textarea>
 
     @error('description')
