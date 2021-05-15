53d630852d353b977f1a0719a102c90d00351da6
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-15 13:47:38 +0800

    profile

--- app/Http/Controllers/Pages/ProfilController.php
+++ app/Http/Controllers/Pages/ProfilController.php
@@ -8,6 +8,7 @@ use App\Models\Hadist;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class ProfilController extends Controller
 {
